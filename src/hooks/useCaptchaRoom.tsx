d312b42c3dcce61a834fb73c4e37b52eddffb929
Kevin Saldana <ksaldana1@gmail.com>
2024-07-16 16:29:32 -0400

    hardcode to dev partykit

--- src/hooks/useCaptchaRoom.tsx
+++ src/hooks/useCaptchaRoom.tsx
@@ -2,8 +2,8 @@ import usePartySocket from "partysocket/react";
 import { useState } from "react";
 import { GameState, Action } from "../../game/logic";
 
-// const PARTY_HOST = "https://test-pk.ksaldana1.partykit.dev";
-const PARTY_HOST = "http://localhost:1999";
+const PARTY_HOST = "https://test-pk.ksaldana1.partykit.dev";
+// const PARTY_HOST = "http://localhost:1999";
 
 export const useGameRoom = (username: string, roomId: string) => {
   const [gameState, setGameState] = useState<GameState | null>(null);
